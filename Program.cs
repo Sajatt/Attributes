7bb9a9d6ea80cec89a84c7a665a1fb8816249fd8
Sajatt <kinkysajatt@gmail.com>
2016-12-02 22:15:59 +0300

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace sw
+{
+
+    class Program
+    {
+        [Firm("Nestle", "USA")]
+        static void Main(string[] args)
+        {    GetAttribute(typeof(Chocolate));
+
+            System.Console.ReadLine();
+
+        }
+
+        public static void GetAttribute(Type t) {
+            Firm myAttribute = (Firm)Attribute.GetCustomAttribute(t, typeof(Firm));
+            Console.WriteLine("Firm: {0}, Country: {1}", myAttribute.name, myAttribute.country);
+
+        
+            Chocolate aa = new Chocolate(30, "Milk", 200);
+            aa.Output();
+            Console.ReadLine();
+        }
+        //ListTest a = new ListTest();
+        //a.OutSorted(a.ListCollect());
+        //Console.ReadLine();
+            
+
+            }
+        }
+    
+        
+                    //Console.ReadLine();
+
+                    //ListTest a = new ListTest();
+                    //a.ListCollect();
+
+                    // ListTest b = new ListTest();
+                    //a.OutSorted(a.ListCollect());
+
+                    //Dark bb = new Dark();
+                    //bb.Outputd();
+                    
+             
+             
+            
+        
+
